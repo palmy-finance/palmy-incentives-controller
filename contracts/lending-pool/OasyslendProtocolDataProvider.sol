23e9dc999fe3357dd11d9bdd8d7520e25ecbc58d
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-10-29 14:59:37 +0900

    fix tests

--- contracts/lending-pool/OasyslendProtocolDataProvider.sol
+++ contracts/lending-pool/OasyslendProtocolDataProvider.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.12;
 
-import '../../submodule-protocol/contracts/misc/OasysLendProtocolDataProvider.sol';
+import '../../submodule-protocol/contracts/misc/OasyslendProtocolDataProvider.sol';
