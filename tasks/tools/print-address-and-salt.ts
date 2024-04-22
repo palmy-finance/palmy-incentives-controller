69164217df386ea0bc9f09c45ea5262c977d45f0
hide-yoshi <hide-yoshi@users.noreply.github.com>
2024-04-22 20:29:39 +0900

    fix todo

--- tasks/tools/print-address-and-salt.ts
+++ tasks/tools/print-address-and-salt.ts
@@ -3,9 +3,11 @@ import { getDb } from '../../helpers/misc-utils';
 
 task('print-address-and-salt', 'print-address-and-salt').setAction(async ({}, localBRE) => {
   const db = getDb();
-  const dbJson = db.toJSON();
+  let dbJson = db.toJSON();
   Object.keys(dbJson).forEach((key) => {
     if (dbJson[key].oasys === undefined) return;
     console.log(`${key},${dbJson[key].oasys?.salt},${dbJson[key].oasys?.address}`);
+    dbJson[key].testnet.address = dbJson[key].oasys?.address;
   });
+  db.write(dbJson);
 });
